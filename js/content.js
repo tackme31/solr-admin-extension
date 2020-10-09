a7d2bc5f740be9b1a02cc1421971f128fcb47077
Takumi Yamada <xirtardauq@gmail.com>
2020-10-09 18:13:35 +0900

    Add project files.

---
+++ js/content.js
@@ -0,0 +1,27 @@
+(function () {
+    const start = () => {
+        const content = document.querySelector('#content > [id]').id
+        switch (content) {
+            case 'query':
+                enableQueryHistory()
+        }
+    }
+
+    const observer = new MutationObserver(mutations => {
+        const contentNode = mutations
+            .map(mutation => Array.from(mutation.addedNodes))
+            .reduce((a, b) => a.concat(b))
+            .find(node => node.id === 'content')
+        if (!contentNode) {
+            return
+        }
+
+        start()
+    })
+
+    const main = document.getElementById('main')
+    const options = { childList: true, subtree: true }
+    observer.observe(main, options)
+
+    start()
+})()
